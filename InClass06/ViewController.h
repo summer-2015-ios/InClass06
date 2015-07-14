78cf7b473dbf232355fef8e619122bcf31f57b04
student <student@ccibio104-206.uncc.edu>
2015-07-14 17:14:45 -0400

    Initial Commit

---
+++ InClass06/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  InClass06
+//
+//  Created by student on 7/14/15.
+//  Copyright (c) 2015 student. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
